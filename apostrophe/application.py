03dd7c6eea8ed6ffa7f8b55542bd67afb12c3ba0
Manuel Genovés <manuel.genoves@gmail.com>
2025-05-01 18:27:00 +0200

    log a warning when trying to open a non-existent file

--- apostrophe/application.py
+++ apostrophe/application.py
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
 
+import logging
+
 import gi
 
 gi.require_version('Gtk', '4.0')
@@ -28,6 +30,7 @@ from apostrophe.main_window import MainWindow
 from apostrophe.settings import Settings
 from apostrophe.theme_switcher import Theme
 
+LOGGER = logging.getLogger('apostrophe')
 
 class Application(Adw.Application):
 
@@ -149,6 +152,7 @@ class Application(Adw.Application):
                                         not window.did_change, self.get_main_windows()))
         for i, file in enumerate(files):
             if not file.query_exists(None):
+                LOGGER.warning(f"{file.get_path()} doesn't exist")
                 continue
 
             c = False
