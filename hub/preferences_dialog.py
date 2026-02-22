132a6aa6e78da36e610269672407573e093df9d5
Fede654 <buzondefede@gmail.com>
2026-02-22 04:24:59 -0300

    fix(ui): remove legacy format combo boxes to prevent PreferencesDialog from crashing

--- hub/preferences_dialog.py
+++ hub/preferences_dialog.py
@@ -19,97 +19,21 @@ import webbrowser
 
 import gi
 
-from hub import helpers
-
 gi.require_version('Gtk', '4.0')
 import logging
 
 from gi.repository import Adw, Gio, GObject, Gtk
-
-logger = logging.getLogger('hub')
-
-from gettext import gettext as _
-
-from hub.preview_security import PreviewSecurity
 from hub.settings import Settings
 
-
-class InputFormat(GObject.Object):
-    __gtype_name__ = "InputFormat"
-
-    name = GObject.Property(type=str)
-    format = GObject.Property(type=str)
-    help = GObject.Property(type=str)
-
-    def __init__(self, name, format, help, **kwargs):
-        super().__init__(**kwargs)
-        self.name: str = name
-        self.format: str = format
-        self.help: str = help
-
-class SecurityFormat(GObject.Object):
-    __gtype_name__ = "SecurityFormat"
-
-    id = GObject.Property(type=int)
-    name = GObject.Property(type=str)
-
-    def __init__(self, id, name, **kwargs):
-        super().__init__(**kwargs)
-        self.id: int = id
-        self.name: str = name
+logger = logging.getLogger('hub')
 
 @Gtk.Template(resource_path='/io/fede/ClaudeDossier/ui/Preferences.ui')
 class ApostrophePreferencesDialog(Adw.PreferencesDialog):
 
     __gtype_name__ = "ApostrophePreferencesDialog"
 
-    formats = [
-        {
-            "name": "Pandoc's Markdown",
-            "format": "markdown",
-            "help": "https://pandoc.org/MANUAL.html#pandocs-markdown"
-        },
-        {
-            "name": "CommonMark",
-            "format": "commonmark",
-            "help": "https://commonmark.org"
-        },
-        {
-            "name": "GitHub Flavored Markdown",
-            "format": "gfm",
-            "help": "https://help.github.com/en/categories/writing-on-github"
-        },
-        {
-            "name": "MultiMarkdown",
-            "format": "markdown_mmd",
-            "help": "https://fletcherpenney.net/multimarkdown"
-        },
-        {
-            "name": "Plain Markdown",
-            "format": "markdown_strict",
-            "help": "https://daringfireball.net/projects/markdown"
-        }
-    ]
-
-    security_levels = [
-        {
-            "id": PreviewSecurity.ASK,
-            "name": _("Always Ask")
-        },
-        {
-            "id": PreviewSecurity.RESTRICTED,
-            "name": _("Always Use Restricted Preview")
-        },
-        {
-            "id": PreviewSecurity.UNRESTRICTED,
-            "name": _("Always Use Unrestricted Preview")
-        },
-    ]
-
     autohide_headerbar_row = Gtk.Template.Child()
     spellcheck_row = Gtk.Template.Child()
-    input_format_comborow = Gtk.Template.Child()
-    preview_security_comborow = Gtk.Template.Child()
     bigger_text_row = Gtk.Template.Child()
     enable_claude_row = Gtk.Template.Child()
     enable_codex_row = Gtk.Template.Child()
@@ -119,32 +43,6 @@ class ApostrophePreferencesDialog(Adw.PreferencesDialog):
 
     def __init__(self):
         super().__init__()
-        input_formats = Gio.ListStore.new(InputFormat)
-
-        for i, format in enumerate(self.formats):
-            input_formats.append(InputFormat(format["name"],
-                                             format["format"],
-                                             format["help"]))
-            if (format["format"] == self.settings.get_string("input-format")):
-                current_format = i
-
-        self.input_format_comborow.set_model(input_formats)
-
-        if current_format:
-            self.input_format_comborow.set_selected(current_format)
-
-        security_levels = Gio.ListStore.new(SecurityFormat)
-
-        for i, level in enumerate(self.security_levels):
-            security_levels.append(SecurityFormat(level["id"],
-                                             level["name"]))
-            if (level["id"] == self.settings.get_enum("preview-security")):
-                current_security_level = i
-
-        self.preview_security_comborow.set_model(security_levels)
-
-        if current_security_level:
-            self.preview_security_comborow.set_selected(current_security_level)
 
         self.settings.bind("autohide-headerbar",
                            self.autohide_headerbar_row,
@@ -175,18 +73,3 @@ class ApostrophePreferencesDialog(Adw.PreferencesDialog):
                            self.enable_antigravity_row,
                            "active",
                            Gio.SettingsBindFlags.DEFAULT)
-
-    @Gtk.Template.Callback()
-    def on_input_format(self, _widget, _index):
-        fmt = self.input_format_comborow.get_selected_item()
-        self.settings.set_string("input-format", fmt.format)
-
-    @Gtk.Template.Callback()
-    def on_input_format_help(self, _):
-        fmt = self.input_format_comborow.get_selected_item()
-        webbrowser.open(fmt.help)
-
-    @Gtk.Template.Callback()
-    def on_security(self, _widget, _index):
-        fmt = self.preview_security_comborow.get_selected_item()
-        self.settings.set_enum("preview-security", fmt.id)
